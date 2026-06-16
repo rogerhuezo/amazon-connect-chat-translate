0b00c81a3e6574f59f0eb76fb14444258142a13a
rogerhuezo <86429445+rogerhuezo@users.noreply.github.com>
2026-06-16 11:54:58 -0700

    Add aws-exports.js with Cognito config

---
+++ src/aws-exports.js
@@ -0,0 +1,9 @@
+const awsconfig = {
+  aws_project_region: "us-east-1",
+  aws_cognito_region: "us-east-1",
+  aws_user_pools_id: "us-east-1_WruT6O3hU",
+  aws_user_pools_web_client_id: "3035en2goiesj1a5seseris0fh",
+  authenticationFlowType: "USER_SRP_AUTH"
+};
+
+export default awsconfig;
