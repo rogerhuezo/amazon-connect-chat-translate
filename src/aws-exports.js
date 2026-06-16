94e2242ce436d4b1e2e37eda893885912b8f64f2
rogerhuezo <86429445+rogerhuezo@users.noreply.github.com>
2026-06-16 12:43:07 -0700

    Add Predictions config for Translate and Comprehend

--- src/aws-exports.js
+++ src/aws-exports.js
@@ -3,7 +3,37 @@ const awsconfig = {
   aws_cognito_region: "us-east-1",
   aws_user_pools_id: "us-east-1_WruT6O3hU",
   aws_user_pools_web_client_id: "3035en2goiesj1a5seseris0fh",
-  authenticationFlowType: "USER_SRP_AUTH"
+  authenticationFlowType: "USER_SRP_AUTH",
+  aws_cognito_identity_pool_id: "us-east-1:4d1c0841-6809-4835-9241-9ada3827be03",
+  predictions: {
+    convert: {
+      translateText: {
+        region: "us-east-1",
+        proxy: false,
+        defaults: {
+          sourceLanguage: "es",
+          targetLanguage: "en"
+        }
+      },
+      speechGenerator: {
+        region: "us-east-1",
+        proxy: false,
+        defaults: {
+          VoiceId: "Ivy",
+          LanguageCode: "en-US"
+        }
+      }
+    },
+    interpret: {
+      interpretText: {
+        region: "us-east-1",
+        proxy: false,
+        defaults: {
+          type: "language"
+        }
+      }
+    }
+  }
 };
 
 export default awsconfig;
